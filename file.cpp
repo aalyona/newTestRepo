5282292bf87538e3e6b43c4e8b82603f7249cf9f
unknown <aantonets@aantonets.integrity.com.ua>
2014-06-13 11:32:59 +0300

    123

--- file.cpp
+++ file.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main(int argc, char** argv) {
-std::cout << “Hello dear world!” << std::endl;
-return 0;
+	std::cout << “Hello dear world!1111111” << std::endl;
+	return 0;
 }
